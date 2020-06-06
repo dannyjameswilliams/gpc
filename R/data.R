5d0bb0db83032b1e55ad9b3aa4c3f44165b61e7c
dannyjameswilliams <dan@danmat.com>
2020-06-06 14:06:25 +0100

    reformatted spam data, added documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Spam dataset
+#'
+#' A dataset containing a collection of spam e-mails, often used in classification examples.
+#'
+#' @usage data(spam)
+#' @format A data frame with 4601 rows and 58 variables.
+#' \describe{
+#' \item{y}{binary response variable: +1 if spam, -1 if not}
+#' \item{X}{57 attributes detailing word frequencies and character frequencies in each e-mail}
+#' }
+#'
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/spambase}
+"spam"
